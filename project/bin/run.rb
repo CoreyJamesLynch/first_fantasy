98d65b2a955f0218b410c58c5cfb0015cc5a6601
Bearinawolfpack <bearinawolfpack@gmail.com>
2020-06-16 10:40:49 -0400

    MVP 90%

--- project/bin/run.rb
+++ project/bin/run.rb
@@ -1,3 +1,37 @@
 require_relative '../config/environment'
 
-puts "hello world"
+puts "Welcome to the dungeon brave soul, what is your name?"
+player_name = gets.chomp
+
+if Player.find_by(player_name: player_name)
+    Hero = Player.find_by(player_name: player_name)
+
+    puts "Welcome back brave soul !"
+else
+    puts "#{player_name} what class of hero are you?"
+    
+    player_class = gets.chomp
+
+    hero = Player.create(player_name: player_name, player_class: player_class)
+end
+
+puts "Brave hero, many monsters threaten this village. Which bounty do you wish to take."
+
+Monster.all.each do |monster|
+    puts monster.monster_name
+end
+
+monster_selection = gets.chomp
+
+monster_object = Monster.find_by(monster_name: monster_selection)
+
+# Encounter.create(player: jenkins, monster: nessie)
+battle = Encounter.create(player: hero , monster: monster_object)
+
+# Encounter.find_by(player: hero, monster: monster_object)
+
+puts "You slew the #{monster_selection}? Fine work #{player_name}! The kind people here in Blue Daisy village can sleep tight now, here is your bounty."
+
+
+
+binding.pry
